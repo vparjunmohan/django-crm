67bd2db5e9c471649ba1bca1ce21312984c85d6a
vparjunmohan <vparjunmohan@gmail.com>
2021-02-03 12:36:11 +0530

    made changes to leads/models.py

--- leads/models.py
+++ leads/models.py
@@ -17,6 +17,9 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
     # phoned = models.BooleanField(default=False)
     # source = models.CharField(choices=SOURCE_CHOICES)
 
@@ -26,3 +29,6 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username 
