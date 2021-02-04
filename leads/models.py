a30f01ed884f0a20ad8d0b5766cd6f3c715e8849
vparjunmohan <vparjunmohan@gmail.com>
2021-02-04 16:11:44 +0530

    added signals to models.py

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.deletion import CASCADE
+from django.db.models.signals import post_save
+
 # Create your models here.
 
 class User(AbstractUser):
@@ -39,3 +40,11 @@ class Agent(models.Model):
     organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     def __str__(self):
         return self.user.email
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    # print(instance, created)
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal, sender=User)
