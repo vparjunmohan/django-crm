095f52c8b2f411464718c016635c12afa5738727
vparjunmohan <vparjunmohan@gmail.com>
2021-02-06 11:24:50 +0530

    agent detail, update and delete views added

--- agents/urls.py
+++ agents/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 
 urlpatterns = [
+    path('agents/<int:pk>/delete/', AgentDeleteView.as_view(), name='agent-delete'),
     path('agents/', AgentListView.as_view(), name='agent-list'),
     path('agents/create/', AgentCreateView.as_view(), name='agent-create'),
+    path('agents/<int:pk>/', AgentDetailView.as_view(), name='agent-detail'),
+    path('agents/<int:pk>/update/', AgentUpdateView.as_view(), name='agent-update'),
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, reverse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
 from .forms import AgentModelForm
@@ -25,3 +25,33 @@ class AgentCreateView(LoginRequiredMixin, CreateView):
         agent.organisation = self.request.user.userprofile 
         agent.save()
         return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'agents/agent_detail.html'
+    context_object_name = 'agent'
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+
+class AgentUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'agents/agent_update.html'
+    form_class = AgentModelForm
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+    def get_success_url(self):
+        return reverse('agent-list')
+
+
+class AgentDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'agents/agent_delete.html'
+    context_object_name = 'agent'
+    
+    def get_success_url(self):
+        return reverse('agent-list')
+
+    def get_queryset(self) :
+        return Agent.objects.all()
