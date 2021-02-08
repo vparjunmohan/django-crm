670ef5c2801a226f61f8faacaac9435ac493c900
vparjunmohan <vparjunmohan@gmail.com>
2021-02-08 11:18:36 +0530

    added category model

--- leads/models.py
+++ leads/models.py
@@ -27,6 +27,8 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -44,6 +46,11 @@ class Agent(models.Model):
     def __str__(self):
         return self.user.username
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 def post_user_created_signal(sender, instance, created, **kwargs):
     # print(instance, created)
